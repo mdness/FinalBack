f38e3ac1fc786ba66d08bb0ef4fcb599356e6c90
mdness <maitecelcastelli@gmail.com>
2022-11-29 15:55:18 -0300

    Add de proyectofinal

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const server = require('./services/server.js')
 const port = 8080;
 
-server.listen(port, () => console.log("Server up en puerto", port));
+server.listen(port, () => console.log(`Server up in: http://localhost:${port}`));
 
 server.on("error", (error) => {
   console.log("Error en servidor", error);
--- src/services/server.js
+++ src/services/server.js
@@ -8,11 +8,4 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", mainRouter);
 
-app.use((req, res, next) => {
-  return res.status(404).json({
-    error: -2,
-    descripcion: "Error",
-  });
-});
-
 module.exports = app;
